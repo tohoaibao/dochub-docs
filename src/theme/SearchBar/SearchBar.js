ea8d2356ade15154c6fc2d5a53ba79da2e87665a
ThuyChau <thuychaunguyen98@gmail.com>
2023-06-28 14:53:16 +0700

    add config searchbar

--- src/theme/SearchBar/SearchBar.js
+++ src/theme/SearchBar/SearchBar.js
@@ -4,13 +4,13 @@ import SearchBar from '@theme-original/SearchBar';
 import { configure } from 'instantsearch.js';
 import { searchBox } from 'instantsearch.js/es/widgets';
 
-export default function SearchBarWrapper(props) {
-  return (
-    <>
-      <SearchBar {...props} />
-    </>
-  ); 
-}
+// export default function SearchBarWrapper(props) {
+//   return (
+//     <>
+//       <SearchBar {...props} />
+//     </>
+//   ); 
+// }
 
 const { search } = configure({
   apiKey: 'aa89dd1d83c67f3432bfee0e3be1b994',
