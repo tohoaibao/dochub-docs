341127b342134e03781b843e6ed7f4527a300623
ThuyChau <thuychaunguyen98@gmail.com>
2023-06-28 15:06:05 +0700

    v7

--- src/theme/SearchBar/SearchBar.js
+++ src/theme/SearchBar/SearchBar.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import SearchBar from '@theme-original/SearchBar';
 
-import { configure } from 'instantsearch.js';
-import { searchBox } from 'instantsearch.js/es/widgets';
+// import { configure } from 'instantsearch.js';
+// import { searchBox } from 'instantsearch.js/es/widgets';
+
+import docsearch from '@docsearch/js';
+import '@docsearch/css';
 
 // export default function SearchBarWrapper(props) {
 //   return (
@@ -12,16 +15,25 @@ import { searchBox } from 'instantsearch.js/es/widgets';
 //   ); 
 // }
 
-const { search } = configure({
-  apiKey: 'aa89dd1d83c67f3432bfee0e3be1b994',
+// const { search } = configure({
+//   apiKey: 'aa89dd1d83c67f3432bfee0e3be1b994',
+//   indexName: 'DocHub_AlgoliaDocSearch',
+// });
+
+// const searchBoxWidget = searchBox({
+//   container: '#searchbox',
+//   placeholder: 'Search...',
+//   autofocus: false,
+// });
+
+// search.addWidget(searchBoxWidget);
+// search.start();
+
+
+
+docsearch({
+  container: '#docsearch',
+  appId: 'JILNIO12KM',
   indexName: 'DocHub_AlgoliaDocSearch',
+  apiKey: 'YOUR_SEARCH_API_KEY',
 });
-
-const searchBoxWidget = searchBox({
-  container: '#searchbox',
-  placeholder: 'Search...',
-  autofocus: false,
-});
-
-search.addWidget(searchBoxWidget);
-search.start();
