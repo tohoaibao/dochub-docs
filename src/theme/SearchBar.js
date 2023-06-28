ae1fa6e925663beaef8a49f563dadf4525464bef
ThuyChau <thuychaunguyen98@gmail.com>
2023-06-28 14:38:21 +0700

    update searchbar v6

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import SearchBar from '@theme-original/SearchBar';
 
+import { configure } from 'instantsearch.js';
+import { searchBox } from 'instantsearch.js/es/widgets';
+
 export default function SearchBarWrapper(props) {
   return (
     <>
       <SearchBar {...props} />
     </>
-  );
+  ); 
 }
+
+const { search } = configure({
+  apiKey: 'aa89dd1d83c67f3432bfee0e3be1b994',
+  indexName: 'DocHub_AlgoliaDocSearch',
+});
+
+const searchBoxWidget = searchBox({
+  container: '#searchbox',
+  placeholder: 'Search...',
+  autofocus: false,
+});
+
+search.addWidget(searchBoxWidget);
+search.start();
